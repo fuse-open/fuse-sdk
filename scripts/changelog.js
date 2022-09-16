7057f53f022a0a5a9b41ca0fbcf1be08276111f4
Morten Daniel Fornes <mdfornes@gmail.com>
2022-09-17 00:08:02 +0700

    add changelog script
    This script fetches and crops changelogs from Uno and Fuselibs, and
    generates a changelog suitable for Fuse SDK.
    
    This eliminates some manual steps normally performed every time we
    release a new version.

---
+++ scripts/changelog.js
@@ -0,0 +1,49 @@
+const axios = require("axios")
+
+const SDK_VERSION = require("../package.json").version
+const UNO_VERSION = "2.4.0"
+const FUSELIBS_VERSION = "2.4.0"
+const LAST_VERSION = "2.3.0"
+
+async function getChangelog(url) {
+    const request = await axios.get(url)
+    const data = request.data
+    const start = data.indexOf("<h3>", data.indexOf("<h2>Changes since"))
+    const end = data.indexOf("</div>", start)
+    return data.substring(start, end)
+}
+
+async function main() {
+    const uno = await getChangelog(`https://github.com/fuse-open/uno/releases/tag/v${UNO_VERSION}`)
+    const fuselibs = await getChangelog(`https://github.com/fuse-open/fuselibs/releases/tag/v${FUSELIBS_VERSION}`)
+    console.log(`### [Install](https://www.npmjs.com/package/fuse-sdk)
+
+\`\`\`
+npm install fuse-sdk@${SDK_VERSION}
+\`\`\`
+
+### Changes since v${LAST_VERSION}
+
+### Fuselibs ${FUSELIBS_VERSION}
+
+<details>
+<summary>Click to see the full changelog!</summary>
+${fuselibs}
+</details>
+
+### Uno ${UNO_VERSION}
+
+<details>
+<summary>Click to see the full changelog!</summary>
+${uno}
+</details>`)
+}
+
+main()
+    .then(() => {
+        process.exit(0)
+    })
+    .catch(error => {
+        console.error(error)
+        process.exit(1)
+    })
