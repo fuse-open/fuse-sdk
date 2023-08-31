a2cb9fda9aad46c52c09899c5f955f94df110af8
Morten Daniel Fornes <mdfornes@gmail.com>
2023-08-31 20:14:51 +0700

    changelog: enumerate patch versions
    Collect changelogs from all patch versions of Uno and Fuselibs.

--- scripts/changelog.js
+++ scripts/changelog.js
@@ -1,8 +1,8 @@
 const axios = require("axios")
 
 const SDK_VERSION = require("../package.json").version
-const UNO_VERSION = "2.9.0"
-const FUSELIBS_VERSION = "2.9.0"
+const UNO_VERSION = "2.9.2"
+const FUSELIBS_VERSION = "2.9.1"
 const LAST_VERSION = "2.8.0"
 
 async function getChangelog(url) {
@@ -13,9 +13,24 @@ async function getChangelog(url) {
     return data.substring(start, end)
 }
 
+/** Returns a list of patch versions, e.g. 2.9.2 => [2.9.2, 2.9.1, 2.9.0] */
+function getPatchVersions(version) {
+    const versions = [version]
+    const versionParts = version.split(".")
+    const patchParts = versionParts[2].split("-")
+
+    for (let patch = Number.parseInt(patchParts[0]); --patch >= 0;) {
+        patchParts[0] = patch.toString()
+        versionParts[2] = patchParts.join("-")
+        versions.push(versionParts.join("."))
+    }
+
+    return versions
+}
+
 async function main() {
-    const uno = await getChangelog(`https://github.com/fuse-open/uno/releases/tag/v${UNO_VERSION}`)
-    const fuselibs = await getChangelog(`https://github.com/fuse-open/fuselibs/releases/tag/v${FUSELIBS_VERSION}`)
+    const uno = await Promise.all(getPatchVersions(UNO_VERSION).map(v => getChangelog(`https://github.com/fuse-open/uno/releases/tag/v${v}`)))
+    const fuselibs = await Promise.all(getPatchVersions(FUSELIBS_VERSION).map(v => getChangelog(`https://github.com/fuse-open/fuselibs/releases/tag/v${v}`)))
     console.log(`### [Install](https://www.npmjs.com/package/fuse-sdk)
 
 \`\`\`
@@ -28,14 +43,14 @@ npm install fuse-sdk@${SDK_VERSION}
 
 <details>
 <summary>Click to see the full changelog</summary>
-${fuselibs}
+${fuselibs.join("\n\n")}
 </details>
 
 ### Uno ${UNO_VERSION}
 
 <details>
 <summary>Click to see the full changelog</summary>
-${uno}
+${uno.join("\n\n")}
 </details>`)
 }
 
